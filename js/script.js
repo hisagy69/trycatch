19e99d9036d86423857601a652cac28d4a911045
Adam <sinegamy11@gmail.com>
2020-08-06 19:33:08 +0700

    add killEval

--- js/script.js
+++ js/script.js
@@ -1,50 +1,59 @@
-const filterByType = (type, ...values) => values.filter(value => typeof value === type),//фильтруются значения соответствующие переданному типу
+const filterByType = (type, values) => { 
+	return values.filter(value => {
+		if (typeof(value) === type) {
+			return true;
+		} 
+		if (!isNaN(+value) && typeof(+value) === type) {
+			return true;
+		}
+	});
+},
 
 	hideAllResponseBlocks = () => {
-		const responseBlocksArray = Array.from(document.querySelectorAll('div.dialog__response-block'));//получили коллекцию элементов и преобразовали в массив
-		responseBlocksArray.forEach(block => block.style.display = 'none');//скрыть все сообщения
+		const responseBlocksArray = Array.from(document.querySelectorAll('div.dialog__response-block'));
+		responseBlocksArray.forEach(block => block.style.display = 'none');
 	},
 
 	showResponseBlock = (blockSelector, msgText, spanSelector) => {
-		hideAllResponseBlocks();//скрыть все блоки
-		document.querySelector(blockSelector).style.display = 'block';//сделает блок видимым
+		hideAllResponseBlocks();
+		document.querySelector(blockSelector).style.display = 'block';
 		if (spanSelector) {
-			document.querySelector(spanSelector).textContent = msgText;//выведет span с сообщением
+			document.querySelector(spanSelector).textContent = msgText;
 		}
 	},
 
-	
-	showError = msgText => showResponseBlock('.dialog__response-block_error', msgText, '#error'),//выведет ошибку, которую передали в catch
 
-	showResults = msgText => showResponseBlock('.dialog__response-block_ok', msgText, '#ok'),//выведет сообщение что есть данные или отсутствуют
+	showError = msgText => showResponseBlock('.dialog__response-block_error', msgText, '#error'),
 
-	showNoResults = () => showResponseBlock('.dialog__response-block_no-results'),//сделает видимым сообщение, что нет результата инпут пустой
+	showResults = msgText => showResponseBlock('.dialog__response-block_ok', msgText, '#ok'),
+
+	showNoResults = () => showResponseBlock('.dialog__response-block_no-results'),
 
 	tryFilterByType = (type, values) => {
 		try {
-			const valuesArray = eval(`filterByType('${type}', ${values})`).join(", ");//функция eval принимает значение инпут дата и фильтрует по типу и объединяет в строку
-			const alertMsg = (valuesArray.length) ?//если есть длинна массива
-				`Данные с типом ${type}: ${valuesArray}` : //выведет данные с соответствующим типом
-				`Отсутствуют данные типа ${type}`; // иначе отсутствуют данные этого типа
-			showResults(alertMsg);//вывести сообщение
+			const valuesArray = filterByType(type, values.replace(/'/g, '').split(', ')).join(', ');
+			const alertMsg = (valuesArray.length) ?
+				`Данные с типом ${type}: ${valuesArray}` :
+				`Отсутствуют данные типа ${type}`;
+			showResults(alertMsg);
 		} catch (e) {
-			showError(`Ошибка: ${e}`);//выведет ошибку
+			showError(`Ошибка: ${e}`);
 		}
 	};
 
-const filterButton = document.querySelector('#filter-btn');//кнопка фильтра
+const filterButton = document.querySelector('#filter-btn');
 
-filterButton.addEventListener('click', e => {//обработчик событий
-	const typeInput = document.querySelector('#type');//селект с типом данных
-	const dataInput = document.querySelector('#data');//инпут с данными
+filterButton.addEventListener('click', e => {
+	const typeInput = document.querySelector('#type');
+	const dataInput = document.querySelector('#data');
 
-	if (dataInput.value === '') {//если инпут пустой
-		dataInput.setCustomValidity('Поле не должно быть пустым!');//выдаст пользовательскую ошибку
-		showNoResults();//выведет сообщение, что инпут пустой
+	if (dataInput.value === '') {
+		dataInput.setCustomValidity('Поле не должно быть пустым!');
+		showNoResults();
 	} else {
-		dataInput.setCustomValidity('');//нет пользовательской ошибки
-		e.preventDefault();//отменить перезагрузку страницы
-		tryFilterByType(typeInput.value.trim(), dataInput.value.trim());//проверит какое сообщение вывести или ошибку
+		dataInput.setCustomValidity('');
+		e.preventDefault();
+		tryFilterByType(typeInput.value.trim(), dataInput.value.trim());
 	}
 });
 
